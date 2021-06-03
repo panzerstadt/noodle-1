b74b390bc361df2d0a68284810d3899c3db67e8c
panzerstadt <tliqun@gmail.com>
2021-06-04 02:44:54 +0900

    animation, particles, light

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,113 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import Noodle1 from "./Noodle1";
-import { Html, OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  Stats,
+} from "@react-three/drei";
+import * as THREE from "three";
+import Ground from "./Ground";
+
+import Particles from "./Particles";
+import {
+  EffectComposer,
+  DepthOfField,
+  Noise,
+  Vignette,
+  SelectiveBloom,
+} from "@react-three/postprocessing";
+import { Resizer, KernelSize } from "postprocessing";
 
 function App() {
+  const mouse = useRef([0, 0]);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  const lightRef = useRef();
+  const bloomRef = useRef();
+
   return (
-    <div id="canvas-container" style={{ height: "100vh" }}>
-      <Canvas shadows camera={{ position: [4, 5, 9], fov: 30 }}>
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.5}
-          enableZoom={false}
-          enablePan={false}
-        />
-        <ambientLight intensity={0.1} />
-        <Suspense fallback={<Html>Loading!</Html>}>
-          <Noodle1 />
-        </Suspense>
-      </Canvas>
+    <div style={{ width: "100%", backgroundColor: "black" }}>
+      <div
+        id="canvas-container"
+        style={{
+          height: "100vh",
+          maxWidth: 1000,
+          margin: "0 auto",
+        }}
+      >
+        <Canvas shadows dpr={[1, 2]}>
+          <Stats />
+          <PerspectiveCamera makeDefault position={[10, 7, -5]} fov={20} />
+          <OrbitControls
+            autoRotate
+            autoRotateSpeed={0.05}
+            enablePan={false}
+            maxAzimuthAngle={Math.PI * 0.75} // right
+            minAzimuthAngle={Math.PI * 0.4} // left
+            minDistance={8}
+            maxDistance={15}
+            maxPolarAngle={Math.PI * 0.4} // up down
+            minPolarAngle={Math.PI * 0.2}
+          />
+          <spotLight
+            ref={lightRef}
+            castShadow
+            intensity={9.5}
+            angle={0.1}
+            penumbra={1}
+            position={[3, 20, 20]}
+            shadow-mapSize-width={1048}
+            shadow-mapSize-height={1048}
+            shadow-camera-far={500}
+            shadow-camera-left={-100}
+            shadow-camera-right={100}
+            shadow-camera-top={100}
+            shadow-camera-bottom={-100}
+          />
+
+          <Suspense fallback={<Html>Loading!</Html>}>
+            <Noodle1 ref={bloomRef} />
+            <Environment preset="sunset" />
+
+            <Particles count={isMobile ? 500 : 3000} mouse={mouse} />
+            <Ground />
+
+            <EffectComposer>
+              <DepthOfField
+                focusDistance={0}
+                focalLength={0.025}
+                bokehScale={5}
+                height={800}
+              />
+              <Noise opacity={0.015} />
+              <Vignette eskil={false} offset={0.1} darkness={0.8} />
+              <SelectiveBloom
+                lights={[lightRef]} // ⚠️ REQUIRED! all relevant lights
+                selection={[bloomRef]} // selection of objects that will have bloom effect
+                selectionLayer={5} // selection layer
+                intensity={2.0} // The bloom intensity.
+                blurPass={undefined} // A blur pass.
+                width={Resizer.AUTO_SIZE} // render width
+                height={Resizer.AUTO_SIZE} // render height
+                kernelSize={KernelSize.SMALL} // blur kernel size
+                luminanceThreshold={0.3} // luminance threshold. Raise this value to mask out darker elements in the scene.
+                luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+              />
+            </EffectComposer>
+          </Suspense>
+
+          <color attach="background" args={["black"]} />
+          <fog
+            color={"black" as unknown as THREE.Color}
+            attach="fog"
+            near={8}
+            far={23}
+          />
+        </Canvas>
+      </div>
     </div>
   );
 }
---
+++ src/Ground.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Reflector, useTexture } from "@react-three/drei";
+
+const Ground = () => {
+  const [floor] = useTexture(["wall_reflection.jpg"]);
+  return (
+    <Reflector
+      receiveShadow
+      resolution={512}
+      args={[20, 20]}
+      mirror={1}
+      mixBlur={5}
+      mixStrength={1}
+      rotation={[-Math.PI / 2, 0, 0]}
+      blur={[400, 100]}
+      position={[-3, -1, 0]}
+    >
+      {(Material, props) => (
+        <Material
+          color="#a0a0a0"
+          metalness={0.5}
+          roughnessMap={floor}
+          // normalMap={normal}
+          // normalScale={[1, 1] as unknown as THREE.Vector2}
+          {...props}
+        />
+      )}
+    </Reflector>
+  );
+};
+
+export default Ground;
